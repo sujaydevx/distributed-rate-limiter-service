6c3e0606cced51e9a82356c2313ed36a8d836d52
sujay-777 <sujaysudharshan7@gmail.com>
2026-07-19 17:12:56 +0530

    feat: define RateLimiterService interface

---
+++ src/main/java/com/fresher/ratelimiter/service/RateLimiterService.java
@@ -0,0 +1,12 @@
+package com.fresher.ratelimiter.service;
+
+import com.fresher.ratelimiter.model.RateLimitResult;
+
+/**
+ * Anything that can answer one question: "should this client's request be allowed?"
+ * Having this as an interface means the rest of the app (the filter) doesn't care
+ * which algorithm is actually running underneath.
+ */
+public interface RateLimiterService {
+    RateLimitResult isAllowed(String clientId);
+}
